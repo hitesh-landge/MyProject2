d5c05959d46a40342dd0df4c94baa0a4853037d9
yashco <yashcosys@yashcos-Mac-mini.local>
2016-10-05 11:44:30 +0530

    First Test
    Des of first Test

---
+++ iOS/iOSTest/iOSTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTest
+//
+//  Created by yashco sys on 05/10/16.
+//  Copyright © 2016 Hitesh Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
