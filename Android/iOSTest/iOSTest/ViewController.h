ee21a105bd5a354639eb6abb6b080ba70f5f64c4
yashco <yashcosys@yashcos-Mac-mini.local>
2016-10-05 11:45:57 +0530

    Android
    Des Android

---
+++ Android/iOSTest/iOSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTest
+//
+//  Created by yashco sys on 05/10/16.
+//  Copyright © 2016 Hitesh Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Android/iOSTest/iOSTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSTest
+//
+//  Created by yashco sys on 05/10/16.
+//  Copyright © 2016 Hitesh Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
